552d6abfad4df117067dd549894f79fac87ebd91
Kennymar Oliveira <kennymar.oliveira.704@ufrn.edu.br>
2026-08-13 11:09:37 -0300

    corrigindo nomes de arquivos no init

--- rag_ocr/pipeline/preprocessors/__init__.py
+++ rag_ocr/pipeline/preprocessors/__init__.py
@@ -1,4 +1,4 @@
 from .document_preprocessor import process_document
 from .image_preprocessor import prepare_image
 
-__all__ = ['process_document', 'prepare_image']
+__all__ = ['document_preprocessor', 'image_preprocessor']
